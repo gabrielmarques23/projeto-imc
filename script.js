24e28b41c7df83609565ac597f63d96a9d61e808
Gabriel Marques <gabrielmarques.if@gmail.com>
2024-01-22 18:02:26 -0300

    Funções do modal

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 // variáveis 
-
 const form = document.querySelector('form');
 const inputWeight = document.querySelector('#weight');
 const inputHeight = document.querySelector('#height');
+
+const Modal = {
+
+    wrapper: document.querySelector('.modal-wrapper'),
+    message: document.querySelector('.modal .tittle span'),
+    buttonClose: document.querySelector('.modal button.close'),
+
+    open() {
+        Modal.wrapper.classList.add('open')
+    },
+    close() {
+        Modal.wrapper.classList.remove('open')
+    },
+}
 
 form.onsubmit = event => {
     event.preventDefault()
@@ -10,8 +23,19 @@ form.onsubmit = event => {
     const weight = inputWeight.value
     const height = inputHeight.value
 
-    console.log(weight, height)
+    const result = IMC(weight, height)
+    const message = `Seu IMC é de ${result}`
+
+    Modal.message.innerText = message
+
+    Modal.open()
 }
 
+Modal.buttonClose.onclick = () => {
+    Modal.close()
+}
 
+function IMC(weight, height) {
+    return (weight / ((height / 100) ** 2)).toFixed(2)
+}
 
