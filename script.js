d5176280d2c70155ac2c6a2b639f8a9adaeb81e4
Gabriel Marques <gabrielmarques.if@gmail.com>
2024-01-22 14:42:30 -0300

    Coloquei as primeiras funções na aplicação

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+// variáveis 
+
+const form = document.querySelector('form');
+const inputWeight = document.querySelector('#weight');
+const inputHeight = document.querySelector('#height');
+
+form.onsubmit = event => {
+    event.preventDefault()
+
+    const weight = inputWeight.value
+    const height = inputHeight.value
+
+    console.log(weight, height)
+}
+
+
 
