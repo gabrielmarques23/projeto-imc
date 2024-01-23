43e44d5eb6f6272b9ade335a698aa8f68dd6c71c
Gabriel Marques <gabrielmarques.if@gmail.com>
2024-01-23 16:58:37 -0300

    Modulei o modal da aplicação
    Criei uma pasta para colocar todos os documentos js

--- js/script.js
+++ js/script.js
@@ -1,21 +1,9 @@
+import { Modal } from './modal.js'
+
 // variáveis 
 const form = document.querySelector('form');
 const inputWeight = document.querySelector('#weight');
 const inputHeight = document.querySelector('#height');
-
-const Modal = {
-
-    wrapper: document.querySelector('.modal-wrapper'),
-    message: document.querySelector('.modal .tittle span'),
-    buttonClose: document.querySelector('.modal button.close'),
-
-    open() {
-        Modal.wrapper.classList.add('open')
-    },
-    close() {
-        Modal.wrapper.classList.remove('open')
-    },
-}
 
 form.onsubmit = event => {
     event.preventDefault()
@@ -29,10 +17,6 @@ form.onsubmit = event => {
     Modal.message.innerText = message
 
     Modal.open()
-}
-
-Modal.buttonClose.onclick = () => {
-    Modal.close()
 }
 
 function IMC(weight, height) {
